dc7a06f76834e7badd04ada75882d82fe2b152ea
Akshat Panchal <akshat.coder.04@gmail.com>
2025-05-25 15:34:01 +0530

    Completed with increment, decrement & reset functionality

--- Beginner_level_projects/Counter_app/script.js
+++ Beginner_level_projects/Counter_app/script.js
@@ -2,3 +2,31 @@ let displayCount = document.querySelector("#counter");
 let decrementBtn = document.querySelector("#decrement-btn")
 let incrementBtn = document.querySelector("#increment-btn")
 let resetBtn = document.querySelector("#reset-btn")
+
+let count = 0;
+
+resetBtn.addEventListener("click", () => {
+    // console.log("Reset");
+    if (count === 0) {
+        alert("Already at zero...!");
+    } else {
+        count = 0;
+        displayCount.innerText = count;
+    }
+})
+
+incrementBtn.addEventListener("click", () => {
+    // console.log("Increased");
+    count++;
+    displayCount.innerText = count;
+})
+
+decrementBtn.addEventListener("click", () => {
+    // console.log("Decreased");
+    if (count <= 0) {
+        alert("Can't go negative...!")
+    } else {
+        count--;
+        displayCount.innerText = count;
+    }
+})
