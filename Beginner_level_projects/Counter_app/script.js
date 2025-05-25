0fe4bb049bd1d1b25cfa83c839e572aac34deeec
Akshat Panchal <akshat.coder.04@gmail.com>
2025-05-25 15:25:07 +0530

    Basic UI of Counter App

---
+++ Beginner_level_projects/Counter_app/script.js
@@ -0,0 +1,4 @@
+let displayCount = document.querySelector("#counter");
+let decrementBtn = document.querySelector("#decrement-btn")
+let incrementBtn = document.querySelector("#increment-btn")
+let resetBtn = document.querySelector("#reset-btn")
