755e594b47349a11341bd0783bb0ab76f184c1a1
Akshat Panchal <akshat.coder.04@gmail.com>
2025-05-25 15:50:30 +0530

    Basic UI for BG Color Flipper

---
+++ Beginner_level_projects/Color_flipper/script.js
@@ -0,0 +1,10 @@
+// // Logic for generating random RGB colors
+// function getRandomRGBColor() {
+//     // Generate random values for R, G, and B (0-255)
+//     const r = Math.floor(Math.random() * 256);
+//     const g = Math.floor(Math.random() * 256);
+//     const b = Math.floor(Math.random() * 256);
+    
+//     // Return in the format "rgb(r, g, b)"
+//     return `rgb(${r}, ${g}, ${b})`;
+// }
